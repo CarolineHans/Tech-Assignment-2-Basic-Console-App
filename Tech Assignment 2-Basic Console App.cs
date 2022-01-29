3caf6e3d58f3cc15e26802f6ea2c1d167bfaa4b3
CHADMIN <CHADMIN@DESKTOP-1PUF00R>
2022-01-28 22:14:25 -0500

    Add project files.

---
+++ Tech Assignment 2-Basic Console App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Assignment_2_Basic_Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
